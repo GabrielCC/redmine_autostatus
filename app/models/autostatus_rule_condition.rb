352086790a59fe274fa8f47d6a0997fb24397674
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2014-07-18 10:37:26 +0300

    Update autostatus_rule_condition.rb
    fixed issue when feature has no task defined

--- app/models/autostatus_rule_condition.rb
+++ app/models/autostatus_rule_condition.rb
@@ -17,12 +17,12 @@ class AutostatusRuleCondition < ActiveRecord::Base
   	case rule_type
   	 when RULE_TYPE_ONE
   	 	status = total_valid_children >= 1
-     when RULE_TYPE_ALL
+  	 when RULE_TYPE_ALL
      	total_children =  Issue.where(
   		:parent_id => issue.id, 
   		:tracker_id => tracker_id,
   		).count
-  		status = total_children == total_valid_children 
+  		status = (total_children == total_valid_children && total_children > 0)
   	 end
   	 status
   end
