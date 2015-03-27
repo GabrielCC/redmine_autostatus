419e0a8f332e9418cd93df973b4f6a9e6854f498
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-03-27 16:26:59 +0200

    Removed unrelated routes and i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,2 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-resources :departments
-resources :resources
-resources :issue_resources
-
-post 'resources/settings/trackers', :to => 'resources#trackers'
-post 'resources/settings/workflows', :to => 'resources#workflows'
-
-match 'projects/:project_id/resources', :to => 'project_resources#create', :via => [:post]
-match 'projects/:project_id/resources', :to => 'project_resources#index', :via => [:get]
